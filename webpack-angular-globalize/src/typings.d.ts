eedf926fc7a8733c16324291b739b4f9def328d7
Yury Orlov <mpcteam.kvet@gmail.com>
2016-11-18 14:40:47 +0300

    Webpack example added

---
+++ webpack-angular-globalize/src/typings.d.ts
@@ -0,0 +1,11 @@
+// This file contains ambient module declarations
+
+declare module 'globalize' { 
+    const value: any;
+    export = value;
+}
+
+declare module 'cldr-data/*' { 
+    const value: any;
+    export = value;
+}
---
+++ webpack-angular-globalize/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: './main',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      globalize$: path.resolve(__dirname, 'node_modules/globalize/dist/globalize.js'),
+      globalize: path.resolve(__dirname, 'node_modules/globalize/dist/globalize'),
+      cldr$: path.resolve(__dirname, 'node_modules/cldrjs/dist/cldr.js'),
+      cldr: path.resolve(__dirname, 'node_modules/cldrjs/dist/cldr'),
+    },
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          'awesome-typescript-loader'
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+};
