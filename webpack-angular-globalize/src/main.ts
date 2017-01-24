b7684c6a24d9d68e88f8403781a72e46b463f9e3
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-01-24 17:26:04 +0300

    Сlear up the index.html file from loading anything from node_modules folder

--- webpack-angular-globalize/src/main.ts
+++ webpack-angular-globalize/src/main.ts
@@ -1,3 +1,6 @@
+import 'core-js/client/shim.min.js';
+import 'zone.js/dist/zone.js';
+
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
