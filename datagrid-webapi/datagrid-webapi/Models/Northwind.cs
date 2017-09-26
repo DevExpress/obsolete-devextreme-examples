fbf925afa1af717a87e0e5de9284d7b5471ef53f
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-09-26 14:44:53 +0300

    Clean .NET projects (#28)

--- datagrid-webapi/datagrid-webapi/Models/Northwind.cs
+++ datagrid-webapi/datagrid-webapi/Models/Northwind.cs
@@ -14,28 +14,16 @@ namespace datagrid_webapi.Models
         }
 
         public virtual DbSet<Category> Categories { get; set; }
-        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<Order_Detail> Order_Details { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<Region> Regions { get; set; }
         public virtual DbSet<Shipper> Shippers { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
-        public virtual DbSet<Territory> Territories { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<CustomerDemographic>()
-                .Property(e => e.CustomerTypeID)
-                .IsFixedLength();
-
-            modelBuilder.Entity<CustomerDemographic>()
-                .HasMany(e => e.Customers)
-                .WithMany(e => e.CustomerDemographics)
-                .Map(m => m.ToTable("CustomerCustomerDemo").MapLeftKey("CustomerTypeID").MapRightKey("CustomerID"));
-
             modelBuilder.Entity<Customer>()
                 .Property(e => e.CustomerID)
                 .IsFixedLength();
@@ -44,11 +32,6 @@ namespace datagrid_webapi.Models
                 .HasMany(e => e.Employees1)
                 .WithOptional(e => e.Employee1)
                 .HasForeignKey(e => e.ReportsTo);
-
-            modelBuilder.Entity<Employee>()
-                .HasMany(e => e.Territories)
-                .WithMany(e => e.Employees)
-                .Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
 
             modelBuilder.Entity<Order_Detail>()
                 .Property(e => e.UnitPrice)
@@ -76,23 +59,10 @@ namespace datagrid_webapi.Models
                 .WithRequired(e => e.Product)
                 .WillCascadeOnDelete(false);
 
-            modelBuilder.Entity<Region>()
-                .Property(e => e.RegionDescription)
-                .IsFixedLength();
-
-            modelBuilder.Entity<Region>()
-                .HasMany(e => e.Territories)
-                .WithRequired(e => e.Region)
-                .WillCascadeOnDelete(false);
-
             modelBuilder.Entity<Shipper>()
                 .HasMany(e => e.Orders)
                 .WithOptional(e => e.Shipper)
                 .HasForeignKey(e => e.ShipVia);
-
-            modelBuilder.Entity<Territory>()
-                .Property(e => e.TerritoryDescription)
-                .IsFixedLength();
         }
     }
 }
