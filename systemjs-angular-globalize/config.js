713f66f5d8b79f8c8c3283292de1756175dd1425
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-03-07 14:53:07 +0300

    Update devextreme version

--- systemjs-angular-globalize/config.js
+++ systemjs-angular-globalize/config.js
@@ -9,16 +9,18 @@ System.config({
   },
   map: {
     'app': './src',
-    
+
     '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
     '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
     '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
     '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
     '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
     '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
     '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
     '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-    
+    'tslib': 'npm:tslib/tslib.js',
+
     '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
     '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
     '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
@@ -26,7 +28,7 @@ System.config({
     '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
     '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
     '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-    
+
     'rxjs': 'npm:rxjs',
     'typescript': 'npm:typescript/lib/typescript.js',
 
@@ -42,7 +44,7 @@ System.config({
     'cldr-data': 'npm:cldr-data',
     'json': 'npm:systemjs-plugin-json/json.js',
   },
-  
+
   packages: {
     app: {
       main: './main.ts',
