9435a1433611834d00b9adcdcc03fec8bc3dd6a7
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2018-03-23 10:44:05 +0300

    SSR: add examples for filterBuilder and pivotGrid

--- universal-angular/src/app/grids/grids.component.ts
+++ universal-angular/src/app/grids/grids.component.ts
@@ -12,12 +12,38 @@ export class GridsComponent implements OnInit {
 
   customers: Customer[];
   employees: Employee[];
+  filter: Array<any>;
+  fields: Array<any>;
+  sales: Array<any>;
 
   devAVDataSource:any;
 
   constructor(service: Service) {
     this.customers = service.getCustomers();
     this.employees = service.getEmployees();
+    this.filter = [
+      ["Product_Current_Inventory", "<>", 0],
+      "or",
+      [
+          ["Product_Name", "contains", "HD"],
+          "and",
+          ["Product_Cost", "<", 200]
+      ]
+    ];
+    this.fields = [
+      {
+          dataField: "Product_Name"
+      }, {
+          caption: "Cost",
+          dataField: "Product_Cost",
+          dataType: "number",
+          format: "currency"
+      }, {
+          dataField: "Product_Current_Inventory",
+          dataType: "number",
+          caption: "Inventory"
+      }
+  ];
 
     this.devAVDataSource = {
       store: {
