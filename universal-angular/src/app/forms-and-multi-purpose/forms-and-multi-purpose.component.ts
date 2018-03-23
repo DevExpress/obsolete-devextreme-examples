4b0c24da27841ea567908085628f535d5b651a52
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2018-03-23 17:51:29 +0300

    Add Form widget

--- universal-angular/src/app/forms-and-multi-purpose/forms-and-multi-purpose.component.ts
+++ universal-angular/src/app/forms-and-multi-purpose/forms-and-multi-purpose.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Service, Employee } from './forms-and-multi-purpose.service';
 
 @Component({
   selector: 'app-forms-and-multi-purpose.component',
   templateUrl: './forms-and-multi-purpose.component.html',
-  styleUrls: ['./forms-and-multi-purpose.component.css']
+  styleUrls: ['./forms-and-multi-purpose.component.css'],
+  providers: [Service]
 })
 export class FormsAndMultiPurposeComponent implements OnInit {
+  employee : Employee;
 
-  constructor() { }
+  constructor(service: Service) {
+    this.employee = service.getEmployee();
+  }
 
   ngOnInit() {
   }
