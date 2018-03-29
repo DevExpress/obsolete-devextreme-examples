6a37096f69f045a16c2695b5d9f2f6e5ae689e8b
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-03-29 14:22:29 +0300

    Add Toolbar example

--- universal-angular/src/app/navigation/navigation.component.ts
+++ universal-angular/src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product, Service } from './products.service';
+import notify from 'devextreme/ui/notify';
 
 @Component({
   selector: 'navigation',
@@ -21,6 +22,68 @@ export class NavigationComponent {
     { text: "clients" },
     { text: "orders" },
     { text: "shipment" }
+  ];
+
+  toolbarItems = [
+    {
+        location: 'before',
+        widget: 'dxButton',
+        options: {
+            type: 'back',
+            text: 'Back',
+            onClick: () => {
+                notify('Back button has been clicked!');
+            }
+        }
+    }, {
+        location: 'before',
+        widget: 'dxButton',
+        locateInMenu: 'auto',
+        options: {
+            icon: 'refresh',
+            onClick: () => {
+                notify('Refresh button has been clicked!');
+            }
+        }
+    }, {
+        location: 'center',
+        locateInMenu: 'never',
+        template: () => {
+            return '<div class=\'toolbar-label\'><b>Tom\'s Club</b> Products</div>';
+        }
+    }, {
+        location: 'after',
+        widget: 'dxTextBox',
+        locateInMenu: 'auto'
+    }, {
+        location: 'after',
+        widget: 'dxButton',
+        locateInMenu: 'auto',
+        options: {
+            icon: 'plus',
+            onClick: () => {
+                notify('Add button has been clicked!');
+            }
+        }
+    }, {
+        locateInMenu: 'always',
+        text: 'Save',
+        onClick: () => {
+            notify('Save option has been clicked!');
+        }
+    }, {
+        locateInMenu: 'always',
+        text: 'Print',
+        onClick: () => {
+            notify('Print option has been clicked!');
+        }
+    }, {
+        locateInMenu: 'always',
+        text: 'Settings',
+        onClick: () => {
+            notify('Settings option has been clicked!');
+        }
+    }
   ];
 
   constructor(service: Service) {
