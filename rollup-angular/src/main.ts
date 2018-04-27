d980acd73a4f20da1662a8653795cd8bdebd1b8e
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-04-27 13:20:16 +0300

    Update dependencies

--- rollup-angular/src/main.ts
+++ rollup-angular/src/main.ts
@@ -2,6 +2,6 @@ import 'core-js/client/shim.min.js';
 import 'zone.js/dist/zone.js';
 
 import { platformBrowser }    from '@angular/platform-browser';
-import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
+import { AppModuleNgFactory } from './app/app.module.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
