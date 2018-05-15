e3cdbb33fa2f1fab54ee4a12c8f24b74cbcc6f58
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-05-15 17:01:20 +0300

    Add Webpack React Example

---
+++ webpack-react/button-example.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Button } from "devextreme-react/ui/button";
+
+export default class extends React.Component {
+    render() {
+        return (
+            <Button icon="plus" text="Click me"/>
+        );
+    }
+}
