4adddcfaac94b2cc1db7947ac36d29fd8f49d3ae
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-05-16 14:35:00 +0300

    Add webpack-react example (#60)

---
+++ webpack-react/app.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.compact.css";
+
+import ButtonExample from "./button-example";
+
+ReactDOM.render(
+  <div>
+    <ButtonExample />
+  </div>,
+  document.getElementById("app")
+);
---
+++ webpack-react/webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  entry: "./app.jsx",
+  output: {
+    filename: "./public/js/app/bundle.js",
+  },
+  devServer: {
+    port: 9900,
+    open: true,
+    openPage: "public/index.html"
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".vue", ".js", ".jsx"]
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ["transform-react-jsx"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      { 
+        test: /\.(eot|svg|ttf|woff|woff2)$/, 
+        use: "url-loader?name=[name].[ext]"
+      }
+    ]
+  }
+};
