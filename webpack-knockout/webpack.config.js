3a89fc1b50d8235c152e44843c5d47a4d469f55d
CORP/zhernovkova.anna <anna.zhernovkova@devexpress.com>
2018-05-21 17:20:32 +0300

    Update webpack version

--- webpack-knockout/webpack.config.js
+++ webpack-knockout/webpack.config.js
@@ -2,16 +2,10 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  mode: 'none',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ]
+  }
 };
