69f64751d5e4b42991480952a5dd68ff9044db3b
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-10-15 20:34:33 +0300

    Add TS example (#74)

---
+++ webpack-vue-typescript/components/button-example.vue
@@ -0,0 +1,30 @@
+<template>
+  <dx-button :text='text' icon='plus' ref='button' @click='click'/>
+</template>
+
+<script lang="ts">
+import { DxButton } from "devextreme-vue/ui/button";
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component({
+  components: {
+    DxButton
+  }
+})
+export default class ButtomExample extends Vue {
+  public $refs: Vue["$refs"] & {
+    button?: DxButton
+  } = {};
+
+  text = 'click me';
+
+  click() {
+    const button = this.$refs.button;
+    if(button) {
+      button.icon = button.icon === "plus" ? "box" : "plus";
+    }
+  };
+}
+</script>
+
+</script>
---
+++ webpack-vue-typescript/webpack.config.js
@@ -0,0 +1,54 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: "./index.ts",
+  output: {
+    filename: "./public/js/app/bundle.js",
+  },
+  devServer: {
+    port: 9900,
+    open: true,
+    openPage: "public/index.html"
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".vue", ".js"],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+              esModule: true
+          }
+      },
+	  { 
+          test: /\.tsx?$/, 
+          use: {
+            loader: "ts-loader",
+            options: {
+              appendTsSuffixTo: [/\.vue$/]
+            }
+          }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      { 
+        test: /\.(eot|svg|ttf|woff|woff2)$/, 
+        use: "url-loader?name=[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
