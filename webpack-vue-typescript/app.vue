700b6c7ab6d73d09137a000be9c1614026df48ea
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-10-15 19:49:57 +0300

    Add Vue TS example

---
+++ webpack-vue-typescript/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <button-example/>
+</template>
+
+<script lang="ts">
+
+import ButtonExample from "./components/button-example.vue";
+
+export default {
+    components: {
+        ButtonExample
+    },
+    name: "app"
+}
+</script>
---
+++ webpack-vue-typescript/components/button-example.vue
@@ -0,0 +1,20 @@
+<template>
+  <dx-button :text='text' icon='plus'/>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+import { DxButton } from "devextreme-vue/ui/button";
+
+@Component({
+  components: {
+    DxButton
+  }
+})
+export default class ButtomExample extends Vue {
+  text = 'click me'
+}
+</script>
+
+</script>
