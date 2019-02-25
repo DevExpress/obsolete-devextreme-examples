c45fe6dd9321b4ca65d9595829d361497a4601a7
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2019-02-25 12:27:10 +0300

    Remove webpack angular globalize demo (#90)

--- webpack-angular-globalize/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'core-js/client/shim.min.js';
-import 'zone.js/dist/zone.js';
-
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
