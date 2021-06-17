c9e73a6f57a458efa6138a085f7e2372dc8b2f96
Alexander Kozlovskiy <zedwag@gmail.com>
2021-06-17 17:57:49 +0300

    Change default dialog import to named import (#113)

--- jspm-jquery/index.js
+++ jspm-jquery/index.js
@@ -4,11 +4,11 @@ import 'devextreme/dist/css/dx.light.css!';
 import $ from 'jquery';
 import 'devextreme/integration/jquery';
 import 'devextreme/ui/button';
-import dialog from 'devextreme/ui/dialog';
+import { alert } from 'devextreme/ui/dialog';
 
 $("#myButton").dxButton({
   text: "Say 'Hello world'",
   onClick: function() {
-    dialog.alert('Hello world!', '', false);
+    alert('Hello world!', '', false);
   }
 });
--- jspm-knockout/index.js
+++ jspm-knockout/index.js
@@ -3,14 +3,14 @@ import 'devextreme/dist/css/dx.light.css!';
 
 import ko from 'knockout';
 import 'devextreme/integration/knockout';
-import dialog from 'devextreme/ui/dialog';
+import { alert } from 'devextreme/ui/dialog';
 import 'devextreme/ui/button';
 
 let viewModel = {
   buttonOptions: {
     text: "Say 'Hello world'",
     onClick: function() {
-      dialog.alert('Hello world!', '', false);
+      alert('Hello world!', '', false);
     }
   }
 };
