3625fa804bbeb95f524c0342cfa6b5d14e3e3fe0
skorokhodova.vlada <vlada.skorokhodova@devexpress.com>
2022-06-09 11:00:09 +0300

    Update imports

--- webpack-jquery/index.js
+++ webpack-jquery/index.js
@@ -1,12 +1,12 @@
-var $ = require('jquery');
-var dialog = require('devextreme/ui/dialog');
+import $ from 'jquery';
+import { alert as dialogAlert } from 'devextreme/ui/dialog';
 
-require('devextreme/integration/jquery');
-require('devextreme/ui/button');
+import 'devextreme/integration/jquery';
+import 'devextreme/ui/button';
 
 $("#myButton").dxButton({
   text: "Say 'Hello world'",
   onClick: function() {
-    dialog.alert('Hello world!', '', false);
+    dialogAlert('Hello world!', '', false);
   }
 });
