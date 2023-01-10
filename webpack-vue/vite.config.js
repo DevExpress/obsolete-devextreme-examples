d31ccc6872e5ae4beaed2892ab9d532c9c2c7307
ivanblinov2k17 <ivanblinov2015@mail.ru>
2023-01-10 11:28:29 +0400

    How to bundle devextreme-vue with Vite

---
+++ webpack-vue/vite.config.js
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'main.js'),
+      name: 'DevExtreme Vue Bundle',
+      formats: ['es', 'cjs'],
+      // the proper extensions will be added
+      fileName: 'devextreme-vue-bundle',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
